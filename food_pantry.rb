0a4bdeb3ed81af5af4685acd990853b33dda4b84
Piradeepan Nagarajan <piradeepann@gmail.com>
2020-04-22 10:07:13 -0700

    Added first file

---
+++ food_pantry.rb
@@ -0,0 +1,18 @@
+class FoodPantry
+    def checkStatusInventory
+        raise NotImplementedError, 'This will be implemented in the below class'
+    end
+end
+class Inventory < FoodPantry
+    def checkStatusInventory
+        return self.clone
+    end
+    def Inventory_inside
+        puts "Food Pantry"
+    end
+end
+
+pantryObject = Inventory.new
+
+pObj = pantryObject.checkStatusInventory
+pObj.Inventory_inside
