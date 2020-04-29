946f8e488d0157b76104dcb1345380e67b08117d
Zaiba Fathima <zaibafathima@Zaibas-MacBook-Pro.local>
2020-04-29 14:27:21 -0700

    added page links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,23 @@ class PagesController < ApplicationController
         @content = 'This is the Measuring Impact page'
     end
 
-    
+    def additem
+        @title = 'Add Item';
+        @content = 'This is the Add Item page'
+    end
 
+    def inventory
+        @title = 'Inventory';
+        @content = 'This is the Inventory page'
+    end
+
+    def provider
+        @title = 'provider';
+        @content = 'This is the provider page'
+    end
+
+    def checkout
+        @title = 'checkout';
+        @content = 'This is the checkout page'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'main#index'
   get 'reducingwaste' => 'pages#reducingwaste'
   get 'measuringimpact' => 'pages#measuringimpact'
+  get 'inventory' => 'pages#inventory'
+  get 'provider' => 'pages#provider'
+  get 'additem' => 'pages#additem'
+  get 'checkout' => 'pages#checkout'
   resources :main
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
