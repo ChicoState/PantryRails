84098889cf32b183dffe52274260f67f586a1897
Zaiba Fathima <zaibafathima@Zaibas-MacBook-Pro.local>
2020-05-08 19:31:05 -0700

    adding

--- app/controllers/provider_controller.rb
+++ app/controllers/provider_controller.rb
@@ -3,12 +3,11 @@ class ProviderController < ApplicationController
         # redirect_to(:action=>'new')
         @providers = Provider.all
     end
-end
-
+    
     def new
         @providers = Provider.new
     end
-
+    
     def show
         @providers = Provider.find(params[:id])
     end
@@ -21,4 +20,29 @@ end
         else
             render 'new'
         end
-    end 
+    end
+
+    def edit
+        @providers = Provider.find(params[:id])
+    end
+
+    def update
+        @providers = Provider.find(params[:id])
+        if(@providers.update(providers_params))
+            redirect_to @providers
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @providers = Provider.find(params[:id])
+        @providers.destroy
+
+        redirect_to items_path
+    end
+
+    private def providers_params
+        params.require(:providers).permit(:providername, :description, :totaldonations)
+    end
+end
