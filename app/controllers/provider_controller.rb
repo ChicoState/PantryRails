543a88ba48d8bbf5885316b714d00a9fffccffa9
Zaiba Fathima <zaibafathima@Zaibas-MacBook-Pro.local>
2020-05-08 20:15:22 -0700

    adding

--- app/controllers/provider_controller.rb
+++ app/controllers/provider_controller.rb
@@ -1,48 +1,48 @@
 class ProviderController < ApplicationController
     def index
         # redirect_to(:action=>'new')
-        @providers = Provider.all
+        @provider = Provider.all
     end
     
     def new
-        @providers = Provider.new
+        @provider = Provider.new
     end
     
     def show
-        @providers = Provider.find(params[:id])
+        @provider = Provider.find(params[:id])
     end
 
     def create
         # render plain: params[:additem].inspect
-        @providers = Provider.new(providers_params)
-        if(@providers.save)
-            redirect_to @providers
+        @provider = Provider.new(providers_params)
+        if(@provider.save)
+            redirect_to @provider
         else
             render 'new'
         end
     end
 
     def edit
-        @providers = Provider.find(params[:id])
+        @provider = Provider.find(params[:id])
     end
 
     def update
-        @providers = Provider.find(params[:id])
-        if(@providers.update(providers_params))
-            redirect_to @providers
+        @provider = Provider.find(params[:id])
+        if(@provider.update(providers_params))
+            redirect_to @provider
         else
             render 'edit'
         end
     end
 
     def destroy
-        @providers = Provider.find(params[:id])
-        @providers.destroy
+        @provider = Provider.find(params[:id])
+        @provider.destroy
 
         redirect_to items_path
     end
 
     private def providers_params
-        params.require(:providers).permit(:providername, :description, :totaldonations)
+        params.require(:provider).permit(:providername, :description, :totaldonations)
     end
 end
