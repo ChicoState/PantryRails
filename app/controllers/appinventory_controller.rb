329d03ed6dcfa4d8e69a27bd017ccf34359b3524
Zaiba Fathima <zaibafathima@Zaibas-MacBook-Pro.local>
2020-05-14 23:14:05 -0700

    changes in addinventory conytroller

--- app/controllers/appinventory_controller.rb
+++ app/controllers/appinventory_controller.rb
@@ -14,35 +14,35 @@ class AppinventoryController < ApplicationController
 
     def create
         # render plain: params[:additem].inspect
-        @items = Item.new(items_params)
-        if(@items.save)
-            redirect_to @items
+        @appinventory = AppInventory.new(appinventory_params)
+        if(@appinventory.save)
+            redirect_to @appinventory
         else
             render 'new'
         end
     end
 
     def edit
-        @items = Item.find(params[:id])
+        @appinventory = AppInventory.find(params[:id])
     end
 
     def update
-        @items = Item.find(params[:id])
-        if(@items.update(items_params))
-            redirect_to @items
+        @appinventory = AppInventory.find(params[:id])
+        if(@appinventory.update(appinventory_params))
+            redirect_to @appinventory
         else
             render 'edit'
         end
     end
 
     def destroy
-        @items = Item.find(params[:id])
-        @items.destroy
+        @appinventory = AppInventory.find(params[:id])
+        @appinventory.destroy
 
-        redirect_to items_path
+        redirect_to appinventory_path
     end
 
-    private def items_params
-        params.require(:items).permit(:name, :donor, :quantity, :received, :expiry, :price)
+    private def appinventory_params
+        params.require(:appinventory).permit(:name, :quantity, :date)
     end
 end
