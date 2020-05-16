b7d4236ae14ed06acbcfadffac63ae584ad6573b
poki3m0n <40546070+poki3m0n@users.noreply.github.com>
2020-05-15 22:00:37 -0700

    model scrap

---
+++ app/models/scrap.rb
@@ -0,0 +1,15 @@
+class Scrap < ApplicationRecord
+    belongs_to :user, optional: :true
+    validates :title, presence: true, uniqueness: {message: 'must be unique'}
+    validates :description, presence: true
+    has_many :scrapscomments,dependent: :destroy
+    mount_uploader :image, ImageUploader
+  
+    def self.search(search)
+      if search
+        where(['title ILIKE ?', "%#{search}%"])
+      else
+        all.order(created_at: :asc)
+      end
+    end
+  end
